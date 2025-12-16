e3ce0c714438c837214a6e3e89b61e98943cd466
orlando <orlandoaltuve20@gmail.com>
2025-12-15 23:28:14 -0300

    video retain on failure

--- tests/EjemploMock.spec.ts
+++ tests/EjemploMock.spec.ts
@@ -11,3 +11,21 @@ test("Hace un mock de una fruta que no viene de la API real ", async ({ page }) 
     // Validamos que Melocotón está disponible
     await expect(page.getByText('Melocotón')).toBeVisible();
 });
+
+test('Obtengo la respuesta real y le agrego algo no tan real', async ({ page }) => {
+    // Obtenemos la respuesta y le agregamos un extra
+    await page.route('*/**/api/v1/fruits', async route => {
+        const response = await route.fetch();
+        const json = await response.json();
+        json.push({ name: 'Lionel Messi', id: 200 });
+        // Obtenemos la respuesta real mientras que le agregamos un extra
+        // al objeto JSON que va a estar siendo representado.
+        await route.fulfill({ response, json });
+    });
+
+    // Vamos a la página
+    await page.goto('https://demo.playwright.dev/api-mocking');
+
+    // Validamos que vino la respuesta real con el extra que le sumamos antes
+    await expect(page.getByText('Lionel Messi', { exact: true })).toBeVisible();
+});
